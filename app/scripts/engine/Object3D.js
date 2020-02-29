239be17495247777f505125a7264ba542e96756d
Tom Croasdale <tecroasdale@gmail.com>
2020-02-29 17:52:51 +0000

    Synching body rotations

--- app/scripts/engine/Object3D.js
+++ app/scripts/engine/Object3D.js
@@ -45,7 +45,7 @@ class Component {
   onLateUpdate() {}
 }
 
-class RigidBody extends Component{
+class RigidBody extends Component {
   constructor(obj, bodyShape, mass) {
     super(obj)
     this.bodyShape = bodyShape
@@ -55,6 +55,7 @@ class RigidBody extends Component{
     this.onCreate()
   }
   onCreate() {
+    // this.attachedObject.mesh.useQuaternion = true
     this.body = new CANNON.Body({
       mass: this.mass, // kg
       position: new CANNON.Vec3(this.attachedObject.mesh.position.x,
@@ -68,7 +69,12 @@ class RigidBody extends Component{
     this.attachedObject.mesh.position.x = this.body.position.x
     this.attachedObject.mesh.position.y = this.body.position.y
     this.attachedObject.mesh.position.z = this.body.position.z
-    // console.log(this.body.position)
+    
+    this.attachedObject.mesh.quaternion.x = this.body.quaternion.x
+    this.attachedObject.mesh.quaternion.y = this.body.quaternion.y
+    this.attachedObject.mesh.quaternion.z = this.body.quaternion.z
+    this.attachedObject.mesh.quaternion.w = this.body.quaternion.w
+    // console.log(this.attachedObject.mesh.quaternion)
   }
 }
 
